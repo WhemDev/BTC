20c402fe274943d2136b74a5b1e5bbca1fb2a2ee
Whem <ardagurses2007@gmail.com>
2024-11-13 22:24:47 +0300

    sonamk

--- driver.py
+++ driver.py
@@ -2,14 +2,15 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import requests
+import json
 
 driver = webdriver.Chrome()
 
+# 5818952657
 
 token = "7920129716:AAHzPB_qD_-TQJhAonzk_1pz7qtsLEvXE_4"
 url = f"https://api.telegram.org/bot{token}/sendMessage"
-
-https://api.telegram.org/bot7920129716:AAHzPB_qD_-TQJhAonzk_1pz7qtsLEvXE_4/getUpdates
+url2 = f"https://api.telegram.org/bot{token}/getUpdates"
 
 messageP = "Growth in the number of enrolled participant count is SLOWED; Please pay attention to take manual action."
 messageA = "TIMER ALERT\nThe counter has decreased significantly. Please check and prepare to take action.\nTIME LEFT: \n"
@@ -23,11 +24,28 @@ print("Binance BTC Button Detector")
 print("Welcome to the Binance BTC Button Alerter Bot!")
 print("To get Alerts and Notifications and Informations, please to following steps : ")
 print("1.   Open Telegram")
-print("2.   Start a new chat with the official telegram bot creator @BotFather")
-print("3    Then use /newbot command to create a new bot. Then follow the introductions.")
-print("4.   Next the bot will send you a secret phase of your bot.")
-print("3.   Once you started a new chat, please copy your chat ID and paste it here.")
-chatID = input("Please enter your chat ID : ")
+print("2.   Start a new chat with the our telegram bot @binanceBtcButtonBot")
+print("3    Then use /start command to start a conversation. Then follow the introductions.")
+print("4.   Once you started a new chat, please enter YOUR telegram username. So our bot can send you private messages.")
+
+while not chat_id:
+    username = input("Please enter your Telegram username: ")
+    
+    response = requests.get(url2)
+    data = response.json()
+    
+    for result in data["result"]:
+        if result["message"]["chat"].get("username") == username:
+            chat_id = result["message"]["chat"]["id"]
+            break
+
+    if chat_id:
+        print(f"Chat ID for username '{username}': {chat_id}")
+    else:
+        print(f"User with username '{username}' not found.")
+        print("Please try again. Make sure you have started a conversation with the bot using /start.")
+        time.sleep(1)
+
 print("\n")
 print("Done, now a window of the Binance 1 BTC Buton game will opened and the further introductions will ne shown")
 time.sleep(1)
