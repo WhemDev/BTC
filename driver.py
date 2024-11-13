61c92d9ee6947f80422521ad840192923d8f7c7d
Whem <ardagurses2007@gmail.com>
2024-11-13 21:27:41 +0300

    wait time

--- driver.py
+++ driver.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-# 
 driver = webdriver.Chrome()
 
 
@@ -20,8 +19,10 @@ print("Binance BTC Button Detector")
 print("Welcome to the Binance BTC Button Alerter Bot!")
 print("To get Alerts and Notifications and Informations, please to following steps : ")
 print("1.   Open Telegram")
-print("2.   Start a chat with our bot @binanceBtcButtonBot")
-print("3.   Once you started a new chat, please copy your chat ID and paste it here")
+print("2.   Start a new chat with the official telegram bot creator @BotFather")
+print("3    Then use /newbot command to create a new bot. Then fallow the introductions.")
+print("4.   Next the bot will send you a secret phase of your bot.")
+print("3.   Once you started a new chat, please copy your chat ID and paste it here.")
 chatID = input("Please enter your chat ID : ")
 print("Done, now a window of the Binance 1 BTC Buton game will opened and the further introductions will ne shown")
 time.sleep(2)
@@ -38,6 +39,7 @@ passed = time.perf_counter()
 try:
     # Saat elementini bulun
     while True:
+        waitTime = 30
         timer_element1 = int(driver.find_element(By.XPATH, '//*[@id="__APP"]/div/div[2]/div[2]/div[1]/div[1]/p').text)
         timer_element2 = int(driver.find_element(By.XPATH, '//*[@id="__APP"]/div/div[2]/div[2]/div[1]/div[2]/p').text)
         timer_element3 = int(driver.find_element(By.XPATH, '//*[@id="__APP"]/div/div[2]/div[2]/div[3]/div[1]/p').text)
@@ -53,7 +55,18 @@ try:
         print("Süre:", secondsLeft)
         # Katılımcı sayısı elementini bulun
         print(passed)
-        time.sleep(10)
+        time.sleep(waitTime)
+
+        if 6000 > secondsLeft >= 4500:
+            secondsLeft = 30
+        elif secondsLeft < 4500:
+            waitTime = 20
+        elif secondsLeft < 3000:
+            waitTime = 5
+        elif secondsLeft < 5000:
+            waitTime = 0
+
+
         if passed > 60:
             participant_element = driver.find_element(By.XPATH, '//*[@id="__APP"]/div/div[2]/div[5]/div[2]/div[2]')  # XPath'i sayfa yapısına göre güncelleyin
             participant_text = participant_element.text
