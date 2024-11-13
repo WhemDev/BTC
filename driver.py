8c4c1842f2f5856e5a51ca2ac5cf6e273743c93d
Whem <ardagurses2007@gmail.com>
2024-11-13 17:46:20 +0300

    fix

--- driver.py
+++ driver.py
@@ -11,18 +11,33 @@ driver.get(url)
 
 # Elementlerin yüklenmesi için bir süre bekleyin
 time.sleep(3)  # Gerekirse sayfanın tamamen yüklenmesi için artırılabilir
+print("Binance BTC Button Detector")
+print("PLEASE USE THE OPENED WINDOW AND LOGIN TO YOUR BINANCE ACCOUNT")
+print("AFTER YOU LOGGED IN AND SAW THE GAME PAGE PLEASE PRESS ENTER TO START ")
+print("\n")
+ifready = (input("Type 'anything or simply press enter when you all set : ") == 'READY')
+print(ifready)
 
 try:
+    
+
+
     # Saat elementini bulun
-    timer_element1 = driver.find_element(By.XPATH, '//*[@id="__APP"]/div/div[2]/div[2]/div[1]/div[1]/p').text
-    timer_element2 = driver.find_element(By.XPATH, '//*[@id="__APP"]/div/div[2]/div[2]/div[1]/div[2]/p').text
-    timer_element3 = driver.find_element(By.XPATH, '//*[@id="__APP"]/div/div[2]/div[2]/div[3]/div[1]/p').text
-    timer_element4 = driver.find_element(By.XPATH, '//*[@id="__APP"]/div/div[2]/div[2]/div[3]/div[2]/p').text
+    timer_element1 = int(driver.find_element(By.XPATH, '//*[@id="__APP"]/div/div[2]/div[2]/div[1]/div[1]/p').text)
+    timer_element2 = int(driver.find_element(By.XPATH, '//*[@id="__APP"]/div/div[2]/div[2]/div[1]/div[2]/p').text)
+    timer_element3 = int(driver.find_element(By.XPATH, '//*[@id="__APP"]/div/div[2]/div[2]/div[3]/div[1]/p').text)
+    timer_element4 = int(driver.find_element(By.XPATH, '//*[@id="__APP"]/div/div[2]/div[2]/div[3]/div[2]/p').text)
+
+    print("Value: ")
+    print(timer_element1)
+    print(timer_element2)
+    print(timer_element3)
+    print(timer_element4)
+
     secondsLeft = (timer_element1 * 1000) + (timer_element2 * 100) + (timer_element3 * 10) + (timer_element4)
     print("Süre:", secondsLeft)
-
     # Katılımcı sayısı elementini bulun
-    participant_element = driver.find_element(By.XPATH, '//*[@id="participants"]')  # XPath'i sayfa yapısına göre güncelleyin
+    participant_element = driver.find_element(By.XPATH, '//*[@id="__APP"]/div/div[2]/div[5]/div[2]/div[2]')  # XPath'i sayfa yapısına göre güncelleyin
     participant_text = participant_element.text
     print("Katılımcı Sayısı:", participant_text)
 
